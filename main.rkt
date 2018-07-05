71ee049b5f69961299cfe33aaf65e387ce412b92
Nick Collins <nick@vividx.co.uk>
2018-07-05 11:28:55 -0400

    remove code from eval.rkt

--- main.rkt
+++ main.rkt
@@ -7,119 +7,25 @@
 (require racket/format)
 
 (require "env.rkt")
-
-;; We want to support:
-;; - numbers
-;; - bools
-;; - strings
-;; - lists
-;; - lambdas
-
-(define expression-interface<%>
-  (interface () self-evaluating?))
-
-(define pair%
-  (class object%
-    (init first second)
-    (define fst first)
-    (define snd second)
-    (define/public (get-first) fst)
-    (define/public (get-second) snd)
-    ;(define v value)
-    (super-new)
-    ;(define/public (value) value)
-    (define/public (self-evaluating?)
-      #t)))
-
-(define (pair a b)
-  (new pair% [first a] [second b]))
-
-(define (pair? x)
-  (is-a? x pair%))
-
-(define (pair-first x)
-  (send x get-first))
-
-(define (pair-second x)
-  (send x get-second))
-
-;; Variables
-
-(define variable%
-  (class object%
-    (init symbol)
-    (define var-symbol symbol)
-    (define/public (self-evaluating?) #f)
-    (define/public (eval-in-env env)
-      (lookup-in-env var-symbol))))
-
-;; variables-tests
-
-(let ([env (bind-var-in-env! 'x 10 empty-env)])
-  (check-eq? (lookup-in-env 'x env) 10))
-
-(let* ([env (bind-var-in-env! 'x 10 empty-env)]
-       [env (bind-var-in-env! 'x 12 env)])
-  (check-eq? (lookup-in-env 'x env) 12))
-
-;; Lambdas/functions consist of:
-;; - parameters
-;; - a body
-;; - the environment the function was defined it
-;; and they are evaluated by creating a frame binding the arguments
-;; to the parameter variables and adding that frame as a child to
-;; the environment.
-
-(define function%
-  (class object%
-    (init args body enclosing-env)
-    (super-new)
-    (define f-args args)
-    (define f-body body)
-    (define f-enclosing-env enclosing-env)
-    (define/public (self-evaluating?) #f)
-    (define/public (function-eval params)
-      ;; create new frame
-      ;; TODO - shouldn't be using make-hash here...
-      (define frame (make-hash (map cons f-args params)))
-      (define env (cons frame f-enclosing-env))
-      (eval f-body env))))
-
-(define built-in-function%
-  (class object%
-    (init racket-function)
-    (super-new)
-    (define function racket-function)
-    (define/public (self-evaluating?) #f)
-    (define/public (function-eval params)
-      (apply function params))))]
-
-;; eval-tests-basic
-(check-eq? 1 (eval 1 '()))
-
-(check-eq? #t (eval #t '()))
-
-(check-eq? (pair-first (pair 1 2)) (pair-first (eval (pair 1 2) empty-env)))
-
-(check-eq? (pair-second (pair 1 2)) (pair-second (eval (pair 1 2) empty-env)))
-
-(check-eq? 13 (eval ((lambda (x) (+ x 10)) 3) '()))
+(require "objects.rkt")
 
 ;; The eval function
 
+(define (constant? exp)
+  (or (equal? 'null exp)
+      (equal? 'true exp)
+      (equal? 'false exp)))
+
 (define (self-evaluating? exp)
-  (or (number? exp)
+  (or (constant? exp)
+      (number? exp)
       (string? exp)
       (boolean? exp)
-      ;(send (first exp) self-evaluating?)))
       (is-a? exp pair%)))
+
 
 (define (eval exp env)
   (cond [(self-evaluating? exp) exp]
-        ; constants
-        [(equal? 'null exp) 'null]
-        [(equal? 'true exp) 'true]
-        [(equal? 'false exp) 'false]
         ; variable
         [(symbol? exp) (lookup-in-env exp env)]
         ; if-else
@@ -149,9 +55,20 @@
         ; for anything else do we just eval the parts and then apply?
         [(list? exp) (let ([f (eval (first exp) env)]
                            [args (map (lambda (e) (eval e env)) (rest exp))])
-                       (send f function-eval args))]
+                       (send f function-eval args eval))]
         ; don't know what to do
         [else (error (string-append "Could not evaluate `" (~a exp) "`"))]))
+
+;; eval-tests-basic
+(check-eq? 1 (eval 1 '()))
+
+(check-eq? #t (eval #t '()))
+
+(check-eq? (pair-first (pair 1 2)) (pair-first (eval (pair 1 2) empty-env)))
+
+(check-eq? (pair-second (pair 1 2)) (pair-second (eval (pair 1 2) empty-env)))
+
+(check-eq? 13 (eval ((lambda (x) (+ x 10)) 3) '()))
 
 (let* ([env empty-env]
        [plus (new built-in-function% [racket-function +])]
@@ -167,7 +84,7 @@
 (check-eq? 10 (eval '(if-else true 10 0) empty-env))
 (check-eq? 0 (eval '(if-else false 10 0) empty-env))
 
-(check-eq? 10 (eval '(if-else ((lambda (x) true) "this is ignored") 10 0) empty-env))
+;(check-eq? 10 (eval '(if-else ((lambda (x) true) "this is ignored") 10 0) empty-env))
 
 
 (define base-env
